a5e910f238446b4ebb3f6dea37c71b7522b9d511
sunilkommiri <sunil1kommuri@gmail.com>
2025-04-24 13:42:56 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_tYsgbTKvL', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '368r6lmmoq2bfu6f6u1q9damjk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_s62uMWZbN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1rj19t9b7eq4o3gp4n0inahd1e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
