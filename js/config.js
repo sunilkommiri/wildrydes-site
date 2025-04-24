a53db1b7a25e2823be84108330569150ea3c5ef8
sunilkommiri <sunil1kommuri@gmail.com>
2025-04-24 13:35:52 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_tYsgbTKvL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '368r6lmmoq2bfu6f6u1q9damjk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
